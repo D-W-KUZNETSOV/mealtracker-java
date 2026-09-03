ff6bdb63c49a742cf6b26fb2891fac336f0bb8b2
Dmitryi Kuznetsov <d.w.kuznetsov@yandex.ru>
2026-09-03 21:47:23 +0300

    add skeleton for exception handling (GlobalExceptionHandler, ApiError)

---
+++ mealtracker/src/main/java/com/e/mealtracker/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.e.mealtracker.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    // завтра сюда добавим @ExceptionHandler
+}
