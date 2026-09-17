8eeb1f8f0f2083af2c78ded77f313610fd99e6df
Dmitryi Kuznetsov <d.w.kuznetsov@yandex.ru>
2026-09-17 20:13:46 +0300

    refactor: remove unused UsernameNotFoundException import from StatsService

--- mealtracker/src/main/java/com/e/mealtracker/service/StatsService.java
+++ mealtracker/src/main/java/com/e/mealtracker/service/StatsService.java
@@ -10,7 +10,7 @@ import com.e.mealtracker.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
